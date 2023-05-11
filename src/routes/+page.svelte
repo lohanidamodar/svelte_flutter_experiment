90f02cf59309ac52baf990481a7baa5cb0cb3aef
Damodar Lohani <dlohani48@gmail.com>
2023-05-11 17:44:00 +0545

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { onMount } from "svelte";
+  /**
+   * @type any
+   */
+  let target;
+  onMount(() => {
+    if (window._flutter) {
+      window._flutter.loader.loadEntrypoint({
+        entrypointUrl: "/flutter/main.dart.js",
+        onEntrypointLoaded: async (engineInitializer) => {
+          let appRunner = await engineInitializer.initializeEngine({
+            hostElement: target,
+            assetBase: "/flutter/",
+          });
+          await appRunner.runApp();
+        },
+      });
+
+      target.addEventListener("flutter-initialized", (event) => {
+        console.log(event);
+      });
+    }
+  });
+</script>
+
+<svelte:head>
+  <script src="/flutter/flutter.js"></script>
+</svelte:head>
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<div class="flutter_target" bind:this={target} />
+
+<style>
+  .flutter_target {
+    width: 300px;
+    height: 400px;
+    margin-top: 20px;
+  }
+</style>
